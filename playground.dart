c9ae9cf5f71c156990df9ff50991dee7674debde
jude <ngwajude 161@gmail.com>
2022-04-18 04:24:38 +0100

    imported the IO library. and got input from the user

--- playground.dart
+++ playground.dart
@@ -1,7 +1,15 @@
-void main() {
+import 'dart:io';
+/**void main() {
   var firstName = "Ngwa";
   var lastName = "Jude";
 
   print(firstName + '' + lastName);
 }
-//string concartenation
+//string concartenation **/
+
+main() {
+  stdout.writeln('what is your name');
+  String? name = stdin.readLineSync();
+
+  print('my name is $name');
+}
